767021039217c7ec2d84040c2bd92f1664a77249
alexandre-mace <helloalexandremace@protonmail.com>
2023-06-09 02:09:45 +0200

    Update matomo

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,8 +3,17 @@ import type { AppProps } from "next/app";
 import { Inter } from "@next/font/google";
 
 const inter = Inter({ subsets: ["latin"] });
+import { init } from "@socialgouv/matomo-next";
+import { useEffect } from "react";
+
+const MATOMO_URL = "https://climatelab.matomo.cloud/";
+const MATOMO_SITE_ID = "7";
 
 export default function App({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    init({ url: MATOMO_URL, siteId: MATOMO_SITE_ID });
+  }, []);
+
   return (
     <main className={inter.className}>
       <Component {...pageProps} />
